2859685acf2aac475336d53413bb043b331e2eed
Dominik <dominikhrynkiewicz1@gmail.com>
2024-02-03 15:43:49 +0100

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+let butt_tak = document.querySelector('.option1')
+let butt_nie = document.querySelector('.option2')
+
+butt_nie.addEventListener('mouse',()=>{
+    butt_nie.textContent = "TAK";
+    console.log(1)
+})
