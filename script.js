40e6a5e2470136b8f11ffa60c5cc7aff49d15c7f
Dominik <dominikhrynkiewicz1@gmail.com>
2025-02-12 22:06:20 +0100

    update

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 let butt_y = document.querySelector('.option1')
 let butt_n = document.querySelector('.option2')
-const jumscare = document.querySelector(".jumpscare")
-let audio = document.querySelector("audio")
-let scale = 20
-let margin = 5
-
-let answers = ["Na pewno?","aha?","Przestań!","To nie jest zabawne...","Będzie mi przykro","Popłaczę się zaraz",":<","Przykro mi teraz","Dobra jeszcze raz to pożałujesz!"]
-let i = 0
-
+const math = document.querySelector(".math")
+const input = document.querySelector("input")
+const answer = "(0,R)"
+const site = "https://pl.pornhub.com/view_video.php?viewkey=ph62a380924030e"
+let i=0
 
 
 butt_n.addEventListener('click',()=>{
-    
-    butt_n.innerText = answers[i]
-    i++
-    if(i==answers.length+1){
-        audio.play()
-        jumscare.style.visibility="visible"
+    if(i){
+        if(answer == input.value){
+            window.open(site)
+        }else
+        alert("Błędna Odpowiedź!!!")
     }
-    scale+=5
-    margin-=0.5
-    butt_n.style.marginLeft = margin+"vw"
-    butt_y.style.padding = scale+"px"; 
-    butt_y.style.fontSize = scale+"px"; 
-    butt_y.style.marginRight = margin+"vw"
+    math.style.opacity="1"
+    i++
 })
